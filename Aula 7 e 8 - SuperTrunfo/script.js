92264c7622ba8a1dbc85a496e74baf62d9fd88a2
LucasLM1 <lucascorreia325@gmail.com>
2021-09-29 14:50:55 -0300

    Aulas 7 e 8 completas

--- Aula 7 e 8 - SuperTrunfo/script.js
+++ Aula 7 e 8 - SuperTrunfo/script.js
@@ -1,5 +1,8 @@
 var carta1 = {
     nome: "Mago",
+
+    imagem: "https://www.clashofclans-dicas.com/wp-content/uploads/2020/12/super-mago-clash-of-clans-super-tropa-superwizard.png",
+
     atributos: {
         ataque: 10,
         defesa: 5,
@@ -9,6 +12,8 @@ var carta1 = {
 
 var carta2 = {
     nome: "Elfo",
+    imagem: "https://www.clashofclans-dicas.com/wp-content/uploads/2020/03/goblin-ardiloso-clash-of-clans-wiki-super-tropa.png",
+
     atributos: {
         ataque: 5,
         defesa: 4,
@@ -18,6 +23,8 @@ var carta2 = {
 
 var carta3 = {
     nome: "Golem",
+    imagem: "https://i.pinimg.com/originals/cf/d1/83/cfd183a2988dece6e89d58274a7bab17.png",
+
     atributos: {
         ataque: 3,
         defesa: 10,
@@ -42,8 +49,16 @@ function sortearCarta() {
 
     document.getElementById("btnSortear").disabled = true;
     document.getElementById("btnJogar").disabled = false;
+    exibirCartaJogador();
+}
 
-    exibirOpcoes();
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName("atributo");
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value;
+        }
+    }
 }
 
 function exibirOpcoes() {
@@ -71,17 +86,73 @@ function obtemAtributoSelecionado() {
 }
 
 function jogar() {
+    console.log("chamou");
     var atributoSelecionado = obtemAtributoSelecionado();
-    var elementoResultado = document.getElementById("resultado");
-    var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
-    var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+    var divResultado = document.getElementById("resultado");
 
-    if (valorCartaJogador > valorCartaMaquina) {
-        elementoResultado.innerHTML = "Você venceu";
-    } else if (valorCartaMaquina > valorCartaJogador) {
-        elementoResultado.innerHTML = "Você perdeu, a carta da máquina é maior";
+    if (
+        cartaJogador.atributos[atributoSelecionado] >
+        cartaMaquina.atributos[atributoSelecionado]
+    ) {
+        htmlResultado = "<p class='resultado-final'>Venceu</p>";
+    } else if (
+        cartaJogador.atributos[atributoSelecionado] <
+        cartaMaquina.atributos[atributoSelecionado]
+    ) {
+        htmlResultado = "<p class='resultado-final'>Perdeu</p>";
     } else {
-        elementoResultado.innerHTML = "Empatou";
+        htmlResultado = "<p class='resultado-final'>Empatou</p>";
     }
-    console.log(cartaMaquina);
+    divResultado.innerHTML = htmlResultado;
+
+    document.getElementById("btnJogar").disabled = true;
+    exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador");
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`; //usa essa crase por causa da concatenação
+    // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+    var moldura =
+        '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+    var opcoesTexto = "";
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto +=
+            "<input type='radio' name='atributo' value='" +
+            atributo +
+            "'>" +
+            atributo +
+            " " +
+            cartaJogador.atributos[atributo] +
+            "<br>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+    divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina");
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+    // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+    var moldura =
+        '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+    var opcoesTexto = "";
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto +=
+            "<p type='text' name='atributo' value='" +
+            atributo +
+            "'>" +
+            atributo +
+            " " +
+            cartaMaquina.atributos[atributo] +
+            "</p>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+    divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
 }
